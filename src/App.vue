0056f9078a541024a2120ed34522bff4de300c3c
fernandapadilha <vitoriafernandaifc@gmail.com>
2024-07-05 11:16:46 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import ExpandBox from './components/ExpandBox.vue'
+</script>
+
+<template>
+  <div>
+    <expand-box />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/ExpandBox.vue
@@ -0,0 +1,27 @@
+<script setup>
+  import { computed, ref } from 'vue';
+
+  const showContent = ref(false);
+  const buttonText = computed(() =>
+    showContent.value ? 'Esconder' : 'Mostrar',
+  );
+</script>
+
+<template>
+  
+  <button @click=" showContent =! showContent ">{{ buttonText }}</button>
+  <div v-if="showContent" class="expand-box">
+    Conteúdo da caixa de expansão.
+  </div>
+  
+</template>
+
+<style scoped>
+  .expand-box {
+    height: 100%;
+    padding: 20px;
+    border: 1px solid gray;
+    border-radius: 5px;
+    box-shadow: 3px 3px 2px 2px gray;
+  }
+</style>
